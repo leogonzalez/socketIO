d1905d3aef9e760c68cc8522172e72eebc5648ba
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-04-06 18:00:32 -0300

    connect/dis event handler

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join( __dirname,'/../public');
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('New user connect');
+  socket.on('disconnect', () => {
+    console.log('Disconnection');
+  })
+  // console.log(io);
+})
+
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });

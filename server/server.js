f198137c191429bc8e403e1fd66fc3cdcaa63121
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2018-04-07 16:26:37 -0300

    emit new msg

--- server/server.js
+++ server/server.js
@@ -1,24 +1,32 @@
-const path = require('path');
-const http = require('http');
-const socketIO = require('socket.io');
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
-const publicPath = path.join( __dirname,'/../public');
+const publicPath = path.join(__dirname, "/../public");
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-  console.log('New user connect');
-  socket.on('disconnect', () => {
-    console.log('Disconnection');
-  })
-  // console.log(io);
-})
+io.on("connection", socket => {
+  console.log("New user connect");
+
+  socket.on("disconnect", () => {
+    console.log("Disconnection");
+  });
+
+  socket.on('createMessage', (message) => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
